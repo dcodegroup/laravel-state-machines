cf75d09232fd9f6e638e319401f660e3349198d8
Yohann <yohann@catherine.email>
2023-12-20 22:17:17 +0800

    Create statusables and statuses tables + model

---
+++ src/Models/Status.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dcodegroup\StateMachines\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Status extends Model
+{
+    protected $fillable = [
+        'name',
+    ];
+}
---
+++ src/Models/Statusable.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Dcodegroup\StateMachines\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Statusable extends Model
+{
+    protected $with = [
+        'status'
+    ];
+
+    protected $fillable = [
+        'status_id',
+        'statusable_id',
+        'statusable_type',
+    ];
+
+    public function status(): BelongsTo
+    {
+        return $this->belongsTo(Status::class);
+    }
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn (mixed $value, array $attributes) => $this->status->name,
+        );
+    }
+}
