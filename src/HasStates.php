ea4c3e96f1f655e61f6bbcd9e165f91dd6720221
Yohann <yohann@catherine.email>
2024-01-18 20:10:48 +0800

    Create a default state if we have one configured

--- src/HasStates.php
+++ src/HasStates.php
@@ -9,6 +9,15 @@ use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 trait HasStates
 {
+    protected static string $defaultState = '';
+
+    public static function bootHasStates()
+    {
+        static::created(function ($model) {
+            self::createDefaultState($model);
+        });
+    }
+
     abstract public function state();
 
     public function statusables(): MorphMany
@@ -19,5 +28,22 @@ trait HasStates
     public function status(): BelongsTo
     {
         return $this->belongsTo(Status::class);
+    }
+
+    protected static function createDefaultState($model)
+    {
+        if (! self::$defaultState) {
+            return;
+        }
+
+        $status = Status::where('machine_name', self::$defaultState)->first();
+
+        Statusable::create([
+            'status_id' => $status->id,
+            'statusable_id' => $model->id,
+            'statusable_type' => self::class,
+        ]);
+
+        $model->update(['status_id', $status->id]);
     }
 }
