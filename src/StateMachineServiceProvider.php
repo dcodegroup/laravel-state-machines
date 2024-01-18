89d072aaa5a17cec7a688ac5d9b77bafc7cdd39a
Yohann <yohann@catherine.email>
2024-01-18 09:34:15 +0800

    Autodiscover service provider. Tweak how we publish migrations

--- src/StateMachineServiceProvider.php
+++ src/StateMachineServiceProvider.php
@@ -6,14 +6,14 @@ use Dcodegroup\StateMachines\Commands\MakeStateMachine;
 use Illuminate\Support\ServiceProvider;
 class StateMachineServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function boot(): void
     {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__ . '/../database/migrations/create_statusables_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_statusables_table.php'),
-                __DIR__ . '/../database/migrations/create_statuses_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_statuses_table.php'),
-            ]);
+        $this->publishes([
+            __DIR__ . '/../database/migrations/create_statusables_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_statusables_table.php'),
+            __DIR__ . '/../database/migrations/create_statuses_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_statuses_table.php'),
+        ], 'statuses-migrations');
 
+        if ($this->app->runningInConsole()) {
             $this->commands([
                 MakeStateMachine::class,
             ]);
