9dd5ab01d7ab637adf45840e2fb46641cf81c55c
Yohann <yohann@catherine.email>
2024-01-18 10:13:21 +0800

    Re-order migration

--- src/StateMachineServiceProvider.php
+++ src/StateMachineServiceProvider.php
@@ -9,8 +9,8 @@ class StateMachineServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->publishes([
-            __DIR__ . '/../database/migrations/create_statusables_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_statusables_table.php'),
             __DIR__ . '/../database/migrations/create_statuses_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_statuses_table.php'),
+            __DIR__ . '/../database/migrations/create_statusables_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time() + 1) . '_create_statusables_table.php'),
         ], 'statuses-migrations');
 
         if ($this->app->runningInConsole()) {
